48f122631b2bce2ac7864879b46f7a352ce61485
Rebecca Bollard <117449944@umail.ucc.ie>
2018-03-07 14:07:29 +0000

    final changes

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -30,8 +30,8 @@ session_start()
            <li> <a  href = "../homepage.html">Home    </a> </li>
     
           <li> <a href = "ebusiness/Ebus1.php">Shop</a> </li>
-           <li> <a href = "cloudcontact.html">Contact     </a> </li>
-           <li> <a href = "CloudServices/About_Cloud.html"> Cloud Services</a></li>
+           <li> <a href = "../CloudServices/cloudcontact.html">Contact     </a> </li>
+           <li> <a href = "../CloudServices/About_Cloud.html"> Cloud Services</a></li>
         </ul>
         
         </div>
